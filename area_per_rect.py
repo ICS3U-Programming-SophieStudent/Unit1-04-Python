39e6b514bc0c9553452fee69b6e89e0d52d84d2a
Sophie Student <l_raffin@hotmail.com>
2022-09-18 22:42:16 +0000

    Added basic math and area of a rectangle files

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Ms Raffin
+# Date: Sep. 18, 2022
+# This program calculates the area & perimeter of a 5cm X 3cm rectangle.
+
+
+def main():
+    print("For a rectangle with length 5cm and width 3cm:")
+    print()
+    print("The area is: {}cm^2".format(5 * 3))
+    print("The perimeter is: {}cm".format(2 * (5 + 3)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Ms Raffin
+# Date: Sep. 18, 2022
+# This program does basic math.
+
+
+def main():
+    print("7-3={}".format(7 - 3))
+    print("2+4={}".format(2 + 4))
+    print("8*6={}".format(8 * 6))
+    print("4/2={}".format(4 / 2))
+    print("1/2={}".format(1 / 2))
+    print("2+5*3={}".format(2 + 5 * 3))
+    print("2^3={}".format(2 ^ 3))  # bitwise operator
+    print("2**3={}".format(2**3))  # exponent
+    print("16**0.5={}".format(16**0.5))  # square root
+
+
+if __name__ == "__main__":
+    main()
